7056c10956d7cdbd813b251ad36a5ad3911608b4
EvillKing3284 <158593915+EvillKing3284@users.noreply.github.com>
2025-12-10 23:25:06 -0300

    atualizando - ex070.py

--- ex059b.py
+++ ex059b.py
@@ -1,38 +1,20 @@
-from time import sleep
-
-valor1 = int(input("Digite um valor: "))
-valor2 = int(input("Digite outro valor: "))
-
-opcao = 0
-
-while opcao != 5:
-    print('''\n[ 1 ] somar
-[ 2 ] multiplicar
-[ 3 ] maior
-[ 4 ] novos números
-[ 5 ] sair do programa''')
-
-    opcao = int(input(">>>>>> Qual a sua opção: "))
-
-    if opcao == 1:
-        print(f"A soma entre {valor1} + {valor2} é {valor1 + valor2}")
-
-    elif opcao == 2:
-        print(f"A multiplicação entre {valor1} x {valor2} é {valor1 * valor2}")
-
-    elif opcao == 3:
-        print(f"O maior valor é {max(valor1, valor2)}")
-
-    elif opcao == 4:
-        print("Informe os números novamente:")
-        valor1 = int(input("Digite um valor: "))
-        valor2 = int(input("Digite outro valor: "))
-
-    elif opcao == 5:
-        print("Finalizando...")
-        sleep(1)
-
+valor1 = int(input("Primeiro valor: "))
+valor2 = int(input("Segundo valor: "))
+escolha = 0
+while escolha != 5:
+    print("[ 1 ]somar\n[ 2 ]multiplicar\n[ 3 ]maior\n[ 4 ]novos números\n[ 5 ]sair do programa")
+    escolha = int(input("Qual a sua escolha: "))
+    if escolha == 1:
+        print(f"A soma entre {valor1} e {valor2} é {valor1 + valor2} ")
+    elif escolha == 2:
+        print(f"O produto entre {valor1} e {valor2} é {valor1 * valor2}")
+    elif escolha == 3:
+        valor_geral = max(valor1, valor2)
+        valor_geral2 = min(valor1, valor2)
+        print(f"O maior valor é {valor_geral} e o menor valor é {valor_geral2}")
+    elif escolha == 4:
+        valor1 = int(input("Primeiro valor: "))
+        valor2 = int(input("Segundo valor: "))
     else:
-        print("Opção inválida! Tente novamente.")
-
-print("Fim do programa! Volte sempre!")
+        print("Código incorreto, tente novamente!")
+        #Após revisar e estudar como fazer este código, consegui fazer sozinho, apenas com pequenos erros
