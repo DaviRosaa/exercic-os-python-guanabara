78c5a841308dfdefdd08ca887013a437c23395a9
EvillKing3284 <158593915+EvillKing3284@users.noreply.github.com>
2025-12-15 21:54:02 -0300

    atualizando - ex061pt2.py

--- ex061pt2.py
+++ ex061pt2.py
@@ -1,9 +1,20 @@
 print("Gerador de PA\n-=-=-=-=-=-")
 a1 = int(input("Digite o primeiro termo: "))
 razao = int(input("Digite a razão: "))
+
 termo = a1
 cont = 1
-while cont <= 10:
-    termo += razao
-    print(termo,end=" ")
-    cont = cont + 1
+total = 0
+mais = 10
+
+while mais != 0:
+    total = total + mais
+
+    while cont <= total:
+        termo += razao
+        print(termo, end=" ")
+        cont = cont + 1
+
+    mais = int(input("\nQuer mais quantos termos? "))
+
+
