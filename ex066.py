2138f58f96fe13a3da891cf04c36fbd070b2cf24
EvillKing3284 <158593915+EvillKing3284@users.noreply.github.com>
2025-12-21 14:44:01 -0300

    atualizando066.py

--- ex066.py
+++ ex066.py
@@ -2,7 +2,7 @@ usuario = 0
 soma = 0
 contador = 0
 while usuario != 999:
-    usuario = int(input("Digite um número inteiro: "))
+    usuario = int(input("Digite um número inteiro[999 para parar]: "))
     if usuario == 999:
         break
     soma += usuario
