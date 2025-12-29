a29b44876a550ba3dc101ffe8d8f00c9f2f8d823
EvillKing3284 <158593915+EvillKing3284@users.noreply.github.com>
2025-12-29 08:02:04 -0300

    segunda-forma-calculo_direto-071b.py

---
+++ ex071b.py
@@ -0,0 +1,20 @@
+print("Banco DEV")
+saque = int(input("Quanto voçê quer sacar R$"))
+total = saque
+ced = 50
+while True:
+    if total >= ced:
+        qtd = total // ced
+        print(f"O total de {qtd} cédulas de R${ced} ")
+        total %= ced              #Quanto sobre de resto da divisão da primeira cédula: 50, 20, 10, 5 e 1
+    else:
+        if ced == 50:
+            ced = 20
+        elif ced == 20:
+            ced = 10
+        elif ced == 10:
+            ced = 5
+        elif ced == 5:
+            ced = 1
+        if total == 0:
+            break
