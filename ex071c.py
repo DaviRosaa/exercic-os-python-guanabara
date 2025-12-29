64373d03a566f91191be3afb36d63984090445b9
EvillKing3284 <158593915+EvillKing3284@users.noreply.github.com>
2025-12-29 15:48:18 -0300

    terceira-forma-matematica_pura-071c.py

---
+++ ex071c.py
@@ -0,0 +1,18 @@
+print("Banco DEV")
+saque = int(input("Quanto voçê quer sacar R$"))
+total = saque
+ced = 50
+while True:
+    if total >= ced:
+        qtd = total // ced
+        total = total - (ced * qtd)
+        print(f"O total dê {qtd} cédulas de R${ced} ")
+    else:
+        if ced == 50:
+            ced = 20
+        elif ced == 20:
+            ced = 10
+        elif ced == 10:
+            ced = 5
+        if total == 0:
+            break
