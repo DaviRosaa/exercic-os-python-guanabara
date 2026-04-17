63e30d3751e9e632a777300cbcf6fcb39f6c68e8
Davi Rosa <158593915+DaviRosaa@users.noreply.github.com>
2026-04-17 19:25:23 -0300

    remove

--- ex072.py
+++ ex072.py
@@ -5,4 +5,3 @@ usuario = str(input("Digite um número entre 0 e 20: "))
 while not str(usuario).isnumeric() or not (0 <= int(usuario) <= 20): #1º OBS: Achei interessante o uso do isnumeric junto com not
         usuario = str(input("Tente novamente. Digite um número entre 0 e 20: "))
 print(f"Voçê digitou o número {numeros[int(usuario)]}")
-batata
